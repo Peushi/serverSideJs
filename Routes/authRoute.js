6e35d051192d6ca150490b0120e560bc83278653
Peushi Ariyawansa <peushi.ariyawansa@epita.fr>
2026-04-28 21:18:59 +0200

    fix: remove student validation middleware from auth registration route

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,11 +1,11 @@
 import express from "express"
 import { register, login } from "../Controllers/authController.js"
 import multerConfig from "../Middleware/multer-config.js"
-import { validateStudent } from "../Middleware/validateStudent.js"
+
 
 const router = express.Router()
 
-router.post("/register", multerConfig, validateStudent, register)
+router.post("/register", multerConfig, register)
 router.post("/login", login)
 
 export default router
